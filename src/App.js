6984d90938c0e6c8300e559ab6af6625a1b057d6
Juan Garcia <juanmgarcia098@gmail.com>
2023-01-29 20:45:51 -0400

    Stub functions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import StarShipList from './pages/StarShipList';
+import StarShipPage from './pages/StarShipPage';
 
 function App() {
   return (
     <div>
-
+      <Routes>
+        <Route path='starship' element={<StarShipPage/>}/>
+        <Route path='/' element={<StarShipList/>}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/StarShipList.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+import { getAllStarships } from "../services/sw-api";
+import { Link } from "react-router-dom";
+
+const StarShipList = () => {
+  const [startshipList, setStarShipList] = useState([])
+
+  useEffect(() => {
+    const fetchAllStarships = async () => {
+      const starshipData = await getAllStarships()
+      setStarShipList(starshipData.results)
+    }
+    fetchAllStarships()
+  }, [])
+
+  return (
+    <>
+    <div><h1>Star Wars Starships</h1></div>
+    {startshipList.length ?
+      <>
+        {startshipList.map(starship =>
+          <div key={starship.index}>
+            <Link to='/starships' state={{starship}}>{starship.name}</Link>
+          </div>
+        )}
+      </>
+      :
+      <>
+        <h3>Loading Starships</h3>
+      </>
+    }
+  </>
+)
+}
+
+export default StarShipList;
